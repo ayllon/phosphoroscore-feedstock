25a716214fda8ac05a51a248a29d3cdc37fc482d
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-05-22 14:05:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# About Phosphoros Core 
+
+[![Build Status](https://travis-ci.org/astrorama/conda-phosphoroscore.svg?branch=master)](https://travis-ci.org/astrorama/conda-phosphoroscore)
+
+Home: http://isdc.unige.ch/euclid/phosphoros/ 
+
+Package license: LGPL 3.0
+
+Summary: A photometric redshift determination tool based on a template fitting method. 
+
---
+++ recipe/build.sh
@@ -0,0 +1,30 @@
+set -xe
+
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ -n "${OSX_ARCH}" ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+else
+     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
+BUILD_DIR="${SRC_DIR}/build"
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+
+export CMAKE_PROJECT_PATH="${PREFIX}/lib/cmake/ElementsProject:${PREFIX}/lib64/cmake/ElementsProject"
+
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DCMAKE_BUILD_TYPE:STRING=RELEASE \
+    -DINSTALL_DOC:BOOL=OFF \
+    -DUSE_SPHINX:BOOL=OFF \
+    -DELEMENTS_BUILD_TESTS:BOOL=OFF \
+    -DBoost_NO_BOOST_CMAKE:BOOL=ON \
+    -DELEMENTS_FLAGS_SET=ON \
+    -DM_LIBRARY:STRING="-lm" \
+    ${CMAKE_PLATFORM_FLAGS[@]} \
+    "${SRC_DIR}"
+
+make ${MAKEFLAGS} 
+make install
+
