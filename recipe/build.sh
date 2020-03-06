540f516d6437b2ecc5644c32bcc75845697177a8
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-03-06 13:35:08 +0100

    Bind BINARY_TAG to conda

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,17 @@
 set -xe
 
+COMPILER_VERSION=$(${CXX} --version | head -1 | perl -n -e'/\S+ (\([^\)]+\) ((\d+\.)+\d+))/ && print $2')
+COMPILER_ID=$(echo ${COMPILER_VERSION} | awk 'BEGIN {FS=".";} { printf $1$2 }')
+
 declare -a CMAKE_PLATFORM_FLAGS
 if [[ -n "${OSX_ARCH}" ]]; then
     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+    PLATFORM="darwin"
+    COMPILER=llvm
 else
-     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+    PLATFORM="linux"
+    COMPILER=gcc
 fi
 
 BUILD_DIR="${SRC_DIR}/build"
@@ -12,6 +19,7 @@ mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 
 export CMAKE_PROJECT_PATH="${PREFIX}/lib/cmake/ElementsProject:${PREFIX}/lib64/cmake/ElementsProject"
+export BINARY_TAG="x86_64-${PLATFORM}-${COMPILER}${COMPILER_ID}-opt"
 
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
@@ -19,12 +27,10 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DINSTALL_DOC:BOOL=OFF \
     -DUSE_SPHINX:BOOL=OFF \
     -DELEMENTS_BUILD_TESTS:BOOL=OFF \
-    -DBoost_NO_BOOST_CMAKE:BOOL=ON \
     -DELEMENTS_FLAGS_SET=ON \
     -DM_LIBRARY:STRING="-lm" \
+    -DBoost_NO_BOOST_CMAKE=ON \
     ${CMAKE_PLATFORM_FLAGS[@]} \
     "${SRC_DIR}"
 
-make ${MAKEFLAGS} 
-make install
-
+make install ${MAKEFLAGS}
